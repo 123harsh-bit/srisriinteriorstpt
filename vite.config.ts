1672d1f4543b16e0e27adc7ddced4ea9847550d9
123harsh-bit <memersofbsc@gmail.com>
2026-02-01 12:58:17 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/srisriinteriorstpt/", // ✅ REQUIRED FOR GITHUB PAGES
+  // ❌ REMOVE base for Render
 
   server: {
     host: "::",
